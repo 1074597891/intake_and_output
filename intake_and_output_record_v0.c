e17cdbbef1ce040dda74bb88cd74e78b4d473ba9
root <1074597891@qq.com>
2021-11-04 22:07:06 +0800

    add  display

--- intake_and_output_record_v0.c
+++ intake_and_output_record_v0.c
@@ -2,8 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 
-int   main(void)
-{
+int N;
 struct liquid
 {
 	char name[10]; //出入量类型  name0=尿  name1=水  name2=药   name3=其他入量  
@@ -11,9 +10,9 @@ struct liquid
         int total_intake_and_output_num;  //出入量总量
 
 }
-	liquid[30];
-
-
+liquid[30];
+void input()
+{
 	FILE  *fp; //创建文件指针，根据指针返回值来确定是否完成文件操作过程 
 	int i;
 	if((fp=fopen("num.txt","a+"))==NULL)   //通过fopen创建文件num.txt 文件，如果返回值为0，说明文件创建失败。 
@@ -22,7 +21,6 @@ struct liquid
       		return;
    	}
 	int input_number=10;
-	int N;
 	printf("输入要记录的个数(0-%d):",input_number);
 	scanf("%d",&N);
  	for(i=0;i<N;i++)
@@ -45,6 +43,38 @@ struct liquid
            }
            fclose(fp);
         }
+void read_form_file()
+{
+	int i;
+	system("cls");
+	FILE *fp;
+	if((fp=fopen("num.txt","rb"))==NULL)
+    	{
+      		printf("cannot open the file\n");
 
+       		return;
+    	}
+	for(i=0;i<N;i++)
+	{
+		fread(&liquid[i],sizeof(struct liquid),1,fp);
+    		fclose(fp);
+	}
+}
 
-
+void  display_all() 
+{ 
+	int i; 
+ 	system("cls");
+    	read_form_file(); 
+ 	printf("全部信息：\n"); 
+ 	for(i=0;i<N;i++) 
+ 	{	 
+ 		printf("出入量类型%s" "时间:%s 出入量总量:%d ", liquid[i].name, liquid[i].day, liquid[i].total_intake_and_output_num);  
+     		printf("\n"); 
+ } 
+} 
+int main(void)
+{
+	input();
+	display_all();
+}
